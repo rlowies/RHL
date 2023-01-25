ebe05f10dbfc45b76358a22db297ab13e928cb5d
Ron Lowies <ronlowies@u.boisestate.edu>
2023-01-24 21:59:08 -0700

    loadmore code

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,9 +1,31 @@
+import { ReactNode } from "react";
 import "./pages.scss";
 
+interface WrapperProps {
+  node: React.ReactNode;
+  children?: React.ReactNode;
+}
+
 const Blog = () => {
+  const values: WrapperProps[] = [{ node: <div>Test 1</div> }, { node: <div>Test 2</div> }, { node: <div>Test 3</div> }];
+
+  const Wrapper: React.FC<WrapperProps> = ({ node, children }) => {
+    return (
+      <span>
+        {node}
+        {children}
+      </span>
+    );
+  };
+
+  const renderValues = (i: number = 0): ReactNode => {
+    if (values?.[i] === undefined) return;
+    return <Wrapper {...values[i]}>{renderValues(i + 1)}</Wrapper>
+  }
+
   return (
     <>
-      <div className="page-container">Blog</div>
+      <div className="page-container">{renderValues()}</div>
     </>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,46 @@
+import { useEffect, useState, useCallback } from "react";
 import "./pages.scss";
 
 const Home = () => {
+  const [data, setData] = useState<any>([]);
+  const [fullData, setFullData] = useState<any>([]);
+
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getPostsAsync = useCallback(async () => {
+    setIsLoading(true);
+    setData(await getPosts(`/api/getPosts/page/${page}`));
+  }, [page]);
+
+  useEffect(() => {
+    getPostsAsync();
+  }, [getPostsAsync]);
+
+  useEffect(() => {
+    if (!isLoading) {
+      return;
+    }
+
+    setFullData(fullData.concat(data));
+    setIsLoading(false);
+  }, [data, fullData, isLoading]);
+
+  const getPosts = async (url: string): Promise<string[]> => new Promise((resolve) => resolve(["someStuff"]));
+
   return (
     <>
-      <div className="page-container">Home</div>
+      <div className="page-container">
+        <button
+          onClick={(e) => {
+            setPage(page + 1);
+          }}
+        >
+          More
+        </button>
+        <br />
+        Data: {fullData}
+      </div>
     </>
   );
 };
